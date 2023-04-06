9b9a6c2615f45e54db3cc6be86fdaf0bf8727230
Bogdan <bogdan.pechounov@gmail.com>
2023-04-06 10:24:02 -0400

    delete task

--- angular-crash/src/app/services/task.service.ts
+++ angular-crash/src/app/services/task.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { TASKS } from '../mock-tasks'
+import { Observable, of } from 'rxjs'
+import { Task } from '../Task'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
 }
