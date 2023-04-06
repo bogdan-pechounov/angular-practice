9f912b70d30838d3faabf0bbb4a25ecd9451c5f2
Bogdan <bogdan.pechounov@gmail.com>
2023-04-06 11:49:00 -0400

    router

--- angular-crash/src/app/components/header/header.component.ts
+++ angular-crash/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { Subscription } from 'rxjs'
 import { UiService } from 'src/app/services/ui.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent {
   showAddTask!: boolean
   subscription: Subscription
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService
       .onToggle()
       .subscribe((v) => (this.showAddTask = v))
@@ -20,5 +21,9 @@ export class HeaderComponent {
 
   toggleAddTask() {
     this.uiService.toggleAddTask()
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route
   }
 }
